bac942e3d1a88a2323cb3b941c6f3b18bf12e267
Ehud Adler <easports96@gmail.com>
2018-12-25 22:33:21 -0500

    Fixed test ... again

--- test-grade-notifier.py
+++ test-grade-notifier.py
@@ -8,10 +8,10 @@ print("1. Pylint")
 os.system('pylint grade-notifier.py')
 
 print("2. Testing Diff")
-os.system('python3 {0}grade-notifier.py --test --test_diff'.format(instance_file_url))
+os.system('python3 {0}grade-notifier.py --test=true --test_diff=true'.format(instance_file_url))
 
 print("3. Testing Add/Remove Instance")
-os.system('python3 {0}grade-notifier.py --test --test_add_remove_instance'.format(instance_file_url))
+os.system('python3 {0}grade-notifier.py --test=true --test_add_remove_instance=true'.format(instance_file_url))
 
 print("4. Testing Message Construction")
-os.system('python3 {0}grade-notifier.py --test --test_message_contruction'.format(instance_file_url))
+os.system('python3 {0}grade-notifier.py --test=true --test_message_contruction=true'.format(instance_file_url))
