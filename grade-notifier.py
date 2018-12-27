a988927acff73a405540871f1bc8569fb4c493d3
Ehud Adler <easports96@gmail.com>
2018-12-27 11:54:50 -0500

    Refactored login flow

--- grade-notifier.py
+++ grade-notifier.py
@@ -14,6 +14,7 @@ __status__      = "Production"
 ## Local
 import constants
 import helper
+import cunylogin
 
 ## Remote
 import requests
@@ -153,43 +154,6 @@ def find_changes(old, new):
 def create_instance(session, username, password, number, school_code):
     login(session, username, password)
     start_notifier(session, number, school_code, username, password)
-
-def login(session, username, password):
-    print('[**] Logging in...')
-
-    session.get(CUNY_FIRST_HOME_URL)
-
-    ## AUTH LOGIN
-
-    data = {
-        'usernameH': f'{username}@login.cuny.edu',
-        'username': username,
-        'password': password,
-        'submit': ''
-    }
-    session.post(CUNY_FIRST_AUTH_SUBMIT_URL, data=data)
-
-    ## STUDENT CENTER
-    response = session.get(CUNY_FIRST_STUDENT_CENTER_URL)
-    tree = html.fromstring(response.text)
-    encquery = tree.xpath('//*[@name="enc_post_data"]/@value')[0]
-
-    data = {
-        'enc_post_data': encquery
-    }
-    response = session.post(CUNY_FIRST_LOGIN-URL, data=data)
-
-    tree = html.fromstring(response.text)
-    encreply = tree.xpath('//*[@name="enc_post_data"]/@value')[0]
-
-    data = {
-        'enc_post_data': encreply
-    }
-    session.post(CUNY_FIRST_LOGIN_2_URL, data=data)
-
-    response = session.get(CUNY_FIRST_SIGNED_IN_STUDENT_CENTER_URL)
-    print('[**] Successfully logged in!')
-    return response
 
 
 def refresh(session, school):
