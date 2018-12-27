8e8feca1000a7d16fc810f36c0ad657bb14edac6
Ehud Adler <easports96@gmail.com>
2018-12-27 13:54:29 -0500

    Major fixes + enhancements

--- test-grade-notifier.py
+++ test-grade-notifier.py
@@ -1,19 +1,50 @@
 import os
+import argparse
+from constants import script_path
+from constants import instance_path
 
-instance_file_url = "/home/fa18/313/adeh6562/public_html/grade-notifier/Grade-Notifier/"
 
-print("Starting test suite.....")
+def parse():
+    parser = argparse.ArgumentParser(description='Specify commands for CUNY Grade Notifier Retriever v1.0')
 
-print("1. Pylint")
-os.system('pylint grade-notifier.py')
+    ## Development
+    parser.add_argument('--local')
+    return parser.parse_args()
 
-print("\n2. Testing Diff")
-os.system('python3 {0}grade-notifier.py --test=true --test_diff=true'.format(instance_file_url))
 
-print("\n3. Testing Add/Remove Instance")
-os.system('rm /home/fa18/313/adeh6562/public_html/grade-notifier/test-instances.txt')
-os.system('touch /home/fa18/313/adeh6562/public_html/grade-notifier/test-instances.txt')
-os.system('python3 {0}grade-notifier.py --test=true --test_add_remove_instance=true'.format(instance_file_url))
 
-print("\n4. Testing Message Construction")
-os.system('python3 {0}grade-notifier.py --test=true --test_message_contruction=true'.format(instance_file_url))
+def run_test(args):
+
+    scriptpath = script_path(args.local)
+    instancepath = instance_path(args.local)
+
+    print("Starting test suite.....")
+
+    print("1. Pylint")
+    os.system('pylint grade-notifier.py')
+
+    print("\n2. Testing Diff")
+    os.system('python3 {0}grade-notifier.py --test=true --test_diff=true'.format(scriptpath))
+
+    print("\n3. Testing Add/Remove Instance")
+
+    if os.path.isfile(instancepath):
+        os.system('rm {0}'.format(instancepath))
+
+    os.system('touch {0}'.format(instancepath))
+    os.system('python3 {0}grade-notifier.py --test=true --test_add_remove_instance=true'.format(scriptpath))
+    os.system('rm {0}'.format(instancepath))
+
+    print("\n4. Testing Message Construction")
+    os.system('python3 {0}grade-notifier.py --test=true --test_message_contruction=true'.format(scriptpath))
+
+
+
+def main():
+    args = parse()
+    run_test(args)
+
+
+if __name__ == '__main__':
+    main()
+
