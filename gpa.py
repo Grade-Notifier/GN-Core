46b86d451e75527abc6712b86df5696bab6c01c3
ericshermancs <zensap92@gmail.com>
2019-01-04 11:07:47 -0500

    work further on gpa

--- gpa.py
+++ gpa.py
@@ -9,16 +9,17 @@ __maintainer__  = "Ehud Adler & Akiva Sherman"
 __email__       = "self@ehudadler.com"
 __status__      = "Production"
 
+
 class GPA():
     _term_gpa = 0
     _cumulative_gpa = 0
 
-    def __init__(self,term_gpa=0,cumulative_gpa=0):
+    def __init__(self,term_gpa = 0, cumulative_gpa = 0):
         self._term_gpa = term_gpa
         self._cumulative_gpa = cumulative_gpa
 
     def get_cumulative_gpa(self):
-    	return self._cumulative_gpa
+        return self._cumulative_gpa
 
     def get_term_gpa(self):
-    	return self._term_gpa
+        return self._term_gpa
