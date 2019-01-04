cb18c53aa0867964ff0d32bef615de87b4a01bba
Ehud Adler <easports96@gmail.com>
2019-01-04 14:06:05 -0500

    Started building up stat tracker for tracking grades given by teachers

---
+++ Crypto/hasher.py
@@ -0,0 +1,17 @@
+"""Hasher
+"""
+
+__author__      = "Ehud Adler & Akiva Sherman"
+__copyright__   = "Copyright 2018, The Punk Kids"
+__license__     = "MIT"
+__version__     = "1.0.0"
+__maintainer__  = "Ehud Adler & Akiva Sherman"
+__email__       = "self@ehudadler.com"
+__status__      = "Production"
+
+class Hasher():
+
+    @staticmethod
+    def sha256(str):
+        pass
+        #TODO
