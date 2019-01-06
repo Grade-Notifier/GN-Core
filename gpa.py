6c994aa56b830b85398a3c5b2aceff537179fb5c
Ehud Adler <easports96@gmail.com>
2019-01-05 21:58:44 -0500

    Started fixing pep8 - 2

--- gpa.py
+++ gpa.py
@@ -1,13 +1,13 @@
 """GPA Class
 """
 
-__author__      = "Ehud Adler & Akiva Sherman"
-__copyright__   = "Copyright 2018, The Punk Kids"
-__license__     = "MIT"
-__version__     = "1.0.0"
-__maintainer__  = "Ehud Adler & Akiva Sherman"
-__email__       = "self@ehudadler.com"
-__status__      = "Production"
+__author__ = "Ehud Adler & Akiva Sherman"
+__copyright__ = "Copyright 2018, The Punk Kids"
+__license__ = "MIT"
+__version__ = "1.0.0"
+__maintainer__ = "Ehud Adler & Akiva Sherman"
+__email__ = "self@ehudadler.com"
+__status__ = "Production"
 
 
 class GPA():
@@ -15,7 +15,7 @@ class GPA():
 
     _cumulative_gpa = 0
 
-    def __init__(self,term_gpa = 0, cumulative_gpa = 0):
+    def __init__(self, term_gpa=0, cumulative_gpa=0):
         self._term_gpa = term_gpa
         self._cumulative_gpa = cumulative_gpa
 
@@ -28,15 +28,17 @@ class GPA():
     @staticmethod
     def get_letter_grade(gpa):
         return {
-            'term_gpa' : GPA.convert_float(gpa.get_term_gpa()),
-            'cumulative_gpa' : GPA.convert_float(gpa.get_cumulative_gpa())
+            'term_gpa': GPA.convert_float(gpa.get_term_gpa()),
+            'cumulative_gpa': GPA.convert_float(gpa.get_cumulative_gpa())
         }
 
     @staticmethod
     def get_number_grade(gpa):
         return {
-            'term_gpa' : GPA.convert_letter(GPA.convert_float(gpa.get_term_gpa())),
-            'cumulative_gpa' : GPA.convert_letter(GPA.convert_float(gpa.get_cumulative_gpa()))
+            'term_gpa':
+            GPA.convert_letter(GPA.convert_float(gpa.get_term_gpa())),
+            'cumulative_gpa':
+            GPA.convert_letter(GPA.convert_float(gpa.get_cumulative_gpa()))
         }
 
     @staticmethod
@@ -67,23 +69,17 @@ class GPA():
     @staticmethod
     def convert_letter(l):
         scale = {
-            'A+' : '97 - 100',
-            'A' : '93 - 96',
-            'A-' : '90 - 92',
-            'B+' : '87 - 89',
-            'B' : '83 - 86',
-            'B-' : '80 - 82',
-            'C+' : '77 - 79',
-            'C' : '73 - 76',
-            'C-' : '70 - 72',
-            'D+' : '67 - 69',
-            'D' : '65 - 66',
-            'F' : '0'
+            'A+': '97 - 100',
+            'A': '93 - 96',
+            'A-': '90 - 92',
+            'B+': '87 - 89',
+            'B': '83 - 86',
+            'B-': '80 - 82',
+            'C+': '77 - 79',
+            'C': '73 - 76',
+            'C-': '70 - 72',
+            'D+': '67 - 69',
+            'D': '65 - 66',
+            'F': '0'
         }
         return scale[l]
-
-
-
-
-
-
