3dc286d4dd88ac6de6498b889b7b3febc898eaea
Ehud Adler <easports96@gmail.com>
2019-01-05 22:38:08 -0500

    Fix constants long-strings

--- grade-notifier.py
+++ grade-notifier.py
@@ -498,7 +498,8 @@ def main():
             s = requests.Session()
             s.headers = {
                 'User-Agent':
-                'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36'
+                'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 '
+                + '(KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36'
             }
             username = input(
                 "Enter username: ") if not args.username else args.username
