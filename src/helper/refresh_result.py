f4171f14414cddc5230ecfb462bb44a74d9f58ef
Ehud Adler <easports96@gmail.com>
2019-01-08 20:54:05 -0500

    fixed result is None and refactor

---
+++ src/helper/refresh_result.py
@@ -0,0 +1,15 @@
+"""Refresh Result
+"""
+
+__author__ = "Ehud Adler & Akiva Sherman"
+__copyright__ = "Copyright 2018, The Punk Kids"
+__license__ = "MIT"
+__version__ = "1.0.0"
+__maintainer__ = "Ehud Adler & Akiva Sherman"
+__email__ = "self@ehudadler.com"
+__status__ = "Production"
+
+class RefreshResult():
+    def __init__(self, classes, gpa):
+        self.classes = classes
+        self.gpa = gpa
