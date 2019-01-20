8afd0fe5f45092f4133e6ca3aef208d7d102f5e7
Ehud Adler <easports96@gmail.com>
2019-01-20 01:26:03 -0500

    addes the new CunyFirstAPI

--- src/core/grade_notifier.py
+++ src/core/grade_notifier.py
@@ -39,6 +39,7 @@ import fileinput
 import time
 import logging
 import traceback
+from cunyfirstapi import Locations
 from cunyfirstapi import CUNYFirstAPI
 from bs4 import BeautifulSoup
 from lxml import etree
@@ -167,7 +168,6 @@ def find_changes(old, new):
     return None if len(changelog) == 0 else Changelog(
         changelog, new_gpa)  # always add gpa to the list
 
-
 ###********* Main Program *********###
 
 def create_instance():
@@ -175,55 +175,8 @@ def create_instance():
     start_notifier()
 
 def refresh():
-
-    term = helper.get_semester()
-    response = api.to_current_term_grades(term)
-    tree = BeautifulSoup(response.text, 'lxml')
-    good_html = tree.prettify()
-    soup = BeautifulSoup(good_html, 'html.parser')
-
-    try:
-        table = soup.findAll(
-            'table', attrs={'class': "PSLEVEL1GRIDWBO"})[0]  # get term table
-    except BaseException:
-        table = None
-
-    result = []
-    refresh_result = None
-
-    if table is not None:
-        row_marker = 0
-        for row in table.find_all('tr'):
-            column_marker = 0
-            row_marker += 1
-            columns = row.find_all('td')
-            data = []
-            for column in columns:
-                if row_marker > 1:
-                    data.append(column.get_text())
-                column_marker += 1
-            if len(data) is not 0:
-                new_class = Class(data[0].strip(), data[1].strip(),
-                                  data[2].strip(), data[3].strip(),
-                                  data[4].strip(), data[5].strip())
-                result.append(new_class)
-
-        gpa_stats = soup.findAll(
-            'table', attrs={'class': "PSLEVEL1GRIDWBO"})[1]  # get gpa table
-
-        last_row = gpa_stats.find_all('tr')[-1]
-        print(last_row.find_all('td')[1].get_text())
-        term_gpa_text = last_row.find_all('td')[1].get_text().strip()
-        if not term_gpa_text:
-            term_gpa_text = '-1'
-
-        term_gpa = float(term_gpa_text)
-        cumulative_gpa = float(last_row.find_all('td')[-1].get_text())
-
-        refresh_result = RefreshResult(result, GPA(term_gpa, cumulative_gpa))
-
-    return refresh_result
-
+    actObj = api.move_to(Locations.student_grades)
+    return actObj.grades
 
 def start_notifier():
     counter = 0
