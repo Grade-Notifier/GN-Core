17d574c40888ce98f8bf060652692d223a2c8ce5
Michael Kolber <michaelkolber@users.noreply.github.com>
2019-01-22 23:38:42 -0500

    Add server-generated css

--- Depfiles/depinstall.sh
+++ Depfiles/depinstall.sh
@@ -6,3 +6,8 @@ done < ./Depfiles/dependencies.pip
 while read p; do
 composer require p
 done < ./Depfiles/dependencies.comp
+
+if [“$HOSTNAME” = “venus”] || [“$HOSTNAME” = “mars”] ; then
+ npm install
+ gulp --gulpfile src/site/development-assets/generate-css.js
+fi
---
+++ src/site/development-assets/generate-css.js
@@ -0,0 +1,24 @@
+var gulp = require('gulp');
+
+var autoprefixer = require('autoprefixer');
+var postcss = require('gulp-postcss');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+
+gulp.task('css', function () {
+    var plugins = [
+        autoprefixer({
+            browsers: ['last 2 versions']
+        })
+    ];
+
+    return gulp
+        .src('./scss/*.scss')
+        .pipe(sourcemaps.init())
+            .pipe(sass.sync().on('error', sass.logError))
+            .pipe(postcss(plugins))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('../site-assets/css/'))
+});
+
+gulp.task('default', ['css']);
