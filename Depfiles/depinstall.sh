81f50a70af797282f56f415d27372f3f1e4cd5cd
Michael Kolber <michaelkolber@users.noreply.github.com>
2019-01-23 21:50:07 -0500

    Fix depinstall.sh

--- Depfiles/depinstall.sh
+++ Depfiles/depinstall.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 while read p; do
-pip3 install $p -U
+  pip3 install $p -U
 done < ./Depfiles/dependencies.pip
 
 while read p; do
-composer require p
+  composer require p
 done < ./Depfiles/dependencies.comp
 
-if [“$HOSTNAME” = “venus”] || [“$HOSTNAME” = “mars”] ; then
- npm install
- gulp --gulpfile src/site/development-assets/generate-css.js
+if [ "$HOSTNAME" = "venus" ] || [ "$HOSTNAME" = "mars" ] ; then
+  npm install
+  gulp --gulpfile src/site/development-assets/generate-css.js
 fi
