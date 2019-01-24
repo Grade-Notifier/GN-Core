0f362d3e78ce4ebd78c9ebaed67db81527d75c08
Ehud Adler <easports96@gmail.com>
2019-01-23 22:21:38 -0500

    Updated depinstall for node installation

--- Depfiles/depinstall.sh
+++ Depfiles/depinstall.sh
@@ -1,13 +1,27 @@
 #!/bin/sh
+
+# Python
 while read p; do
-  pip3 install $p -U
+pip3 install $p -U
 done < ./Depfiles/dependencies.pip
 
+# PHP
 while read p; do
-  composer require p
+composer require p
 done < ./Depfiles/dependencies.comp
 
-if [ "$HOSTNAME" = "venus" ] || [ "$HOSTNAME" = "mars" ] ; then
-  npm install
-  gulp --gulpfile src/site/development-assets/generate-css.js
+# Node
+if [[ "$OSTYPE" == "linux-gnu" ]] || [[ "$OSTYPE" == "darwin"* ]]; then
+    curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.1/install.sh | bash;
+    export NVM_DIR="$HOME/.nvm"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+    nvm install node;
+    nvm use node;
 fi
+
+# CSS/Node
+if [ "$HOSTNAME" = "venus" ] || [ "$HOSTNAME" = "mars" ] ; then
+    npm install;
+    gulp --gulpfile src/site/development-assets/generate-css.js;
+fi
+
