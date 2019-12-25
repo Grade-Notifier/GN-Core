c3553dcdee4433c8e588e6cca6a380465db488e5
Ehud Adler <easports96@gmail.com>
2019-12-24 22:02:00 -0500

    added RSA JS->Python scheme

--- index.php
+++ index.php
@@ -1,243 +1,250 @@
 <?php
-    $arr = array();
-    $status = '';
-    $title = '';
-    $message = '';
-    $landing = !isset($_POST["submit"]);
+$arr = array();
+$status = '';
+$title = '';
+$message = '';
+$landing = !isset($_POST["submit"]);
 
-    function display(){
-        global $arr, $status, $title, $message;
-        $cmd = '';
-       
-        $cmd = escapeshellcmd('python3 src/core/initializegn.py --username='.$_POST["username"].' --password='.$_POST["password"].' --school='.$_POST["school"].' --phone='.$_POST["phone"]);
+function display()
+{
+    global $arr, $status, $title, $message;
+    $cmd = '';
 
-        $message = exec($cmd, $arr);
+    $cmd = escapeshellcmd('python3 src/core/initializegn.py --username=' . $_POST["username"] . ' --password=' . $_POST["password"] . ' --school=' . $_POST["school"] . ' --phone=' . $_POST["phone"]);
 
-        for ($x = 0; $x < count($arr); $x++) {
-            if (strpos($arr[$x], 'RENDER::') !== false) {
+    $message = exec($cmd, $arr);
 
-                    // Remove RENDER::
-                $worend = str_replace("RENDER::", "", $arr[$x]);
+    for ($x = 0; $x < count($arr); $x++) {
+        if (strpos($arr[$x], 'RENDER::') !== false) {
 
-                    // Parse the text to get the status and title
-                    //
-                    // Regex matches the following:
-                    // --<key>="<value>" (Will match)
-                    // --<key>=<value> (Will not match)
-                    //
-                    // Contain a single group to capture the
-                    // <value>
-                preg_match_all("/--?[^=\s]+?=\"([^\"]+)\"?/", $worend, $matches);
-                $val = $matches[1];
-                $status = $val[0];
-                $title = $val[1];
+            // Remove RENDER::
+            $worend = str_replace("RENDER::", "", $arr[$x]);
 
+            // Parse the text to get the status and title
+            //
+            // Regex matches the following:
+            // --<key>="<value>" (Will match)
+            // --<key>=<value> (Will not match)
+            //
+            // Contain a single group to capture the
+            // <value>
+            preg_match_all("/--?[^=\s]+?=\"([^\"]+)\"?/", $worend, $matches);
+            $val = $matches[1];
+            $status = $val[0];
+            $title = $val[1];
+
+            $x++;
+            // Continue eating until we see the end indicator
+            // denoted by [END]. This is an extra precaution as
+            // it should always end with the last item
+            // in the array
+            while (strpos($arr[$x], '[END]') === false) {
+                $message .= nl2br(" $arr[$x]\r\n");
                 $x++;
-                    // Continue eating until we see the end indicator
-                    // denoted by [END]. This is an extra precaution as
-                    // it should always end with the last item
-                    // in the array
-                while (strpos($arr[$x], '[END]') === false) {
-                    $message.= nl2br(" $arr[$x]\r\n");
-                    $x++;
-                }
-
-                $message = str_replace("[END]", "", $message);
-                    // We only care about the first returned
-                    // error or success message. Break out early
-                    // to avoid overwritting
-                break;
             }
+
+            $message = str_replace("[END]", "", $message);
+            // We only care about the first returned
+            // error or success message. Break out early
+            // to avoid overwritting
+            break;
         }
     }
+}
 
-    if (isset($_POST["submit"])){
-        display();
-    }
-    ?>
+if (isset($_POST["submit"])) {
+    display();
+}
+?>
 
 <!doctype html>
 <html>
+
 <head>
-<?php
+    <?php
     if (gethostname() == "venus" || gethostname() == "mars") {
         require_once 'vendor/autoload.php';
         $dotenv = Dotenv\Dotenv::create(__DIR__);
         $dotenv->load();
         $mars_user = getenv('MARS_USERNAME');
-        echo '<base href="/~'.$mars_user.'/grade-notifier/Grade-Notifier/">';
+        echo '<base href="/~' . $mars_user . '/grade-notifier/Grade-Notifier/">';
     }
-?>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<title>CUNY Grade Notifier</title>
-<link type="text/css" rel="stylesheet" href="src/site/site-assets/css/styles.css">
-<link href="https://fonts.googleapis.com/css?family=Roboto+Slab:400,700" rel="stylesheet">
+    ?>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>CUNY Grade Notifier</title>
+    <link type="text/css" rel="stylesheet" href="src/site/site-assets/css/styles.css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Slab:400,700" rel="stylesheet">
 
-<!-- Favicon and icons for other platforms -->
-<link rel="apple-touch-icon" sizes="180x180" href="/src/site/site-assets/icons/apple-touch-icon.png">
-<link rel="icon" type="image/png" sizes="32x32" href="/src/site/site-assets/icons/favicon-32x32.png">
-<link rel="icon" type="image/png" sizes="16x16" href="/src/site/site-assets/icons/favicon-16x16.png">
-<link rel="manifest" href="/src/site/site-assets/icons/site.webmanifest">
-<link rel="mask-icon" href="/src/site/site-assets/icons/safari-pinned-tab.svg" color="#5bbad5">
-<link rel="shortcut icon" href="/src/site/site-assets/icons/favicon.ico">
-<meta name="apple-mobile-web-app-title" content="Grade Notifier">
-<meta name="application-name" content="Grade Notifier">
-<meta name="msapplication-TileColor" content="#ffffff">
-<meta name="msapplication-config" content="/src/site/site-assets/icons/browserconfig.xml">
-<meta name="theme-color" content="#ffffff">
-<script src="src/helper/security/forge/dist/forge.js"></script>
+    <!-- Favicon and icons for other platforms -->
+    <link rel="apple-touch-icon" sizes="180x180" href="/src/site/site-assets/icons/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/src/site/site-assets/icons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/src/site/site-assets/icons/favicon-16x16.png">
+    <link rel="manifest" href="/src/site/site-assets/icons/site.webmanifest">
+    <link rel="mask-icon" href="/src/site/site-assets/icons/safari-pinned-tab.svg" color="#5bbad5">
+    <link rel="shortcut icon" href="/src/site/site-assets/icons/favicon.ico">
+    <meta name="apple-mobile-web-app-title" content="Grade Notifier">
+    <meta name="application-name" content="Grade Notifier">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-config" content="/src/site/site-assets/icons/browserconfig.xml">
+    <meta name="theme-color" content="#ffffff">
+    <script src="src/helper/security/forge/dist/forge.js"></script>
 
 </head>
+
 <body>
-<div class="wrapper">
-<div class="column column--left">
-<?php
-    if (!$landing):
-    if ($status == "ok"):
-    ?>
-<img class="status-symbol" alt="Check" src="Assets/site/Check.svg">
-<?php
-    elseif ($status == "error"):
-    ?>
-<img class="status-symbol" alt="Exclamation mark" src="Assets/site/Exclamation.svg">
-<?php
-    endif;
-    endif;
-    ?>
-<h1 class="callout">
-<?php
-    if ($landing):
-    ?>
-Get a text when you<br>get your grades!
-<?php
-    elseif ($title):
-    echo $title;
-    endif;
-    ?>
-</h1>
-<?php
-    if ($landing):
-    ?>
+    <div class="wrapper">
+        <div class="column column--left">
+            <?php
+            if (!$landing) :
+                if ($status == "ok") :
+            ?>
+                    <img class="status-symbol" alt="Check" src="Assets/site/Check.svg">
+                <?php
+                elseif ($status == "error") :
+                ?>
+                    <img class="status-symbol" alt="Exclamation mark" src="Assets/site/Exclamation.svg">
+            <?php
+                endif;
+            endif;
+            ?>
+            <h1 class="callout">
+                <?php
+                if ($landing) :
+                ?>
+                    Get a text when you<br>get your grades!
+                <?php
+                elseif ($title) :
+                    echo $title;
+                endif;
+                ?>
+            </h1>
+            <?php
+            if ($landing) :
+            ?>
 
-<script type="text/javascript">
-    function encryptPassword(){
+                <script type="text/javascript">
+                    function encryptPassword() {
+                        let rsa = forge.pki.rsa;
+                        let publicKeyEncoded = '-----BEGIN PUBLIC KEY-----\
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA1qQpNjXpsJ/BFhqn7HPG\
+upVFCpNYU6nrT1F5fFg3B4lt/oTO3blsl1SQpK2UXwq1Zd8xF2dG6+1OKuOLrDsi\
+Gnd7GcRJy/iXgoTIPnluRLa4FfuzDafZxRGgzZDsHhYSItx2q7NdjCtqADcNs5Pl\
+4JdIYRT2t+lZ6W4HLfjKYVzRqVGLGP/BQItcR6yVP1Pmt9CEEBHQJwuVrP2SYBxk\
+GxrxrYUcAha/n8mKewUIHzJtJmFLli2FlOsIUtgT9H2Qc3ctUExqs6f2FdVuByVf\
+P17+bwRAn+7lx94I7FDDbNuia0tHK8eP0K+cRzvtoFydS6tzvcV0+WlzzP3XJe7j\
+xQIDAQAB\
+-----END PUBLIC KEY-----';
+                        let publicKey = forge.pki.publicKeyFromPem(publicKeyEncoded);
+                        let ciphertext = publicKey.encrypt(document.userform.password.value, 'RSA-OAEP', {
+                            md: forge.md.sha256.create(),
+                            mgf1: { md: forge.md.sha1.create() }
+                        });
+                        document.userform.password.value = forge.util.encode64(ciphertext)
+                    }
+                </script>
 
-    let bytes = document.userform.password.value;
-    let pkeyBytes = "-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAyD0L92P1+ok+6p5gwxtJ\n08e9J/UaBdtgCHp7d/N8hbiTp2WyDAETOQhDhaZC5hBWxS5w7EMsvSfOirv4rDkz\nHhnXlI45AWF1ZOVMvf/jC+1adp4xmxykqg7YeV0mWumz2Y3C/pxPnIhpxPBmo/fk\nDQGpTpNCGabrL474De0810QuXPg8HqhfDN9Rb6ZoOrKZeYlxn3yUKRu0DY2uIhg+\nUSDW4N/RiqZxvfX9T6TY1hmKWcZggz2MfMxFseOeWm3npo86pVPMuQaXnGbsRmaQ\nAbdLdvqd7lMsTrST+dDDCSOzuWvyxkYPKWw0o5jnpJ7lwZr2VidfLM2t5z95F8+2\nQQIDAQAB\n-----END PUBLIC KEY-----";
-    let publicKey = forge.pki.publicKeyFromPem(pkeyBytes);
+                <button onClick="encryptPassword()">Click ME</button>
 
-    let hashes = {
-        md: forge.md.sha256.create(),
-        mgf1: {
-          md: forge.md.sha1.create()
-        }
-    }
-    var encrypted = publicKey.encrypt(bytes, 'RSA-OAEP', hashes);
+                <div class="callout__divider"></div>
+                <form name="userform" action=<?php echo $_SERVER['PHP_SELF']; ?> method="POST" onSubmit="encryptPassword()">
+                    <input class="input" type="text" name="username" placeholder="Username" required><span class="username-posttext">@login.cuny.edu</span>
+                    <br>
+                    <input class="input input--full-width" type="password" name="password" placeholder="Password" required>
+                    <br>
 
-    document.userform.password.value = encrypted;
+                    <!-- <label for="school">School:</label> -->
+                    <select class="input input--select input--full-width" id="school" name="school" required>
+                        <option value="BAR01">Baruch College</option>
+                        <option value="BMC01">Borough of Manhattan CC</option>
+                        <option value="BCC01">Bronx CC</option>
+                        <option value="BKL01">Brooklyn College</option>
+                        <option value="CTY01">City College</option>
+                        <option value="CSI01">College of Staten Island</option>
+                        <option value="GRD01">Graduate Center</option>
+                        <option value="NCC01">Guttman CC</option>
+                        <option value="HOS01">Hostos CC</option>
+                        <option value="HTR01">Hunter College</option>
+                        <option value="JJC01">John Jay College</option>
+                        <option value="KCC01">Kingsborough CC</option>
+                        <option value="LAG01">LaGuardia CC</option>
+                        <option value="LEH01">Lehman College</option>
+                        <option value="MHC01">Macaulay Honors College</option>
+                        <option value="MEC01">Medgar Evers College</option>
+                        <option value="NYT01">NYC College of Technology</option>
+                        <option value="QNS01" selected>Queens College</option>
+                        <option value="QCC01">Queensborough CC</option>
+                        <option value="SOJ01">School of Journalism</option>
+                        <option value="SLU01">School of Labor&Urban Studies</option>
+                        <option value="LAW01">School of Law</option>
+                        <option value="MED01">School of Medicine</option>
+                        <option value="SPS01">School of Professional Studies</option>
+                        <option value="SPH01">School of Public Health</option>
+                        <option value="UAPC1">University Processing Center</option>
+                        <option value="YRK01">York College</option>
+                    </select>
+                    <br>
+                    <input class="input input--full-width" type="text" name="phone" placeholder="Phone Number" required>
+                    <br>
+                    <input class="submit" type="submit" name="submit" value="Text me!">
+                </form>
 
+                <?php
+            else :
+                if ($status == "ok") :
+                ?>
+                    <div class="confirm-message__wrapper">
+                        <?php
+                        echo "<p class=\"confirm-message__text\">";
+                        echo $message;
+                        echo "</p>";
+                        ?>
+                    </div>
+                <?php
+                endif;
 
-}
-</script>
-<div class="callout__divider"></div>
-<form name="userform" action=<?php echo $_SERVER['PHP_SELF']; ?> method="POST" onSubmit="encryptPassword()">
-<input class="input" type="text" name="username" placeholder="Username" required><span class="username-posttext">@login.cuny.edu</span>
-<br>
-<input class="input input--full-width" type="password" name="password" placeholder="Password" required>
-<br>
+                if ($status == "error") :
+                ?>
+                    <div class="confirm-message__wrapper">
+                        <?php
+                        echo "<p class=\"confirm-message__text\">";
+                        echo $message;
+                        echo "</p>";
+                        ?>
+                    </div>
+                    <form method="get">
+                        <input class="submit" type="submit" value="Start over">
+                    </form>
+            <?php
+                endif;
+            endif;
+            ?>
 
-<!-- <label for="school">School:</label> -->
-<select class="input input--select input--full-width" id="school" name="school" required>
-<option value="BAR01">Baruch College</option>
-<option value="BMC01">Borough of Manhattan CC</option>
-<option value="BCC01">Bronx CC</option>
-<option value="BKL01">Brooklyn College</option>
-<option value="CTY01">City College</option>
-<option value="CSI01">College of Staten Island</option>
-<option value="GRD01">Graduate Center</option>
-<option value="NCC01">Guttman CC</option>
-<option value="HOS01">Hostos CC</option>
-<option value="HTR01">Hunter College</option>
-<option value="JJC01">John Jay College</option>
-<option value="KCC01">Kingsborough CC</option>
-<option value="LAG01">LaGuardia CC</option>
-<option value="LEH01">Lehman College</option>
-<option value="MHC01">Macaulay Honors College</option>
-<option value="MEC01">Medgar Evers College</option>
-<option value="NYT01">NYC College of Technology</option>
-<option value="QNS01" selected>Queens College</option>
-<option value="QCC01">Queensborough CC</option>
-<option value="SOJ01">School of Journalism</option>
-<option value="SLU01">School of Labor&Urban Studies</option>
-<option value="LAW01">School of Law</option>
-<option value="MED01">School of Medicine</option>
-<option value="SPS01">School of Professional Studies</option>
-<option value="SPH01">School of Public Health</option>
-<option value="UAPC1">University Processing Center</option>
-<option value="YRK01">York College</option>
-</select>
-<br>
+            <div class="credits">
+                <h3 class="credit">Made with ❤️ by Ehud Adler</h3>
+                <h4 class="credit">Big thanks to @ericshermancs</h4>
+                <h4 class="credit">Please see our <a href="https://github.com/Grade-Notifier/GN-Core/blob/master/CONTRIBUTORS.md" title="Contributors">full list of contributors</a>.</h4>
+            </div>
+        </div>
 
-<input class="input input--full-width" type="text" name="phone" placeholder="Phone Number" required>
-<br>
-<input class="submit" type="submit" name="submit" value="Text me!">
-</form>
-<?php
-    else:
-    if($status == "ok"):
-    ?>
-<div class="confirm-message__wrapper">
-<?php
-    echo "<p class=\"confirm-message__text\">";
-    echo $message;
-    echo "</p>";
-    ?>
-</div>
-<?php
-    endif;
-
-    if ($status == "error"):
-    ?>
-    <div class="confirm-message__wrapper">
-<?php
-    echo "<p class=\"confirm-message__text\">";
-    echo $message;
-    echo "</p>";
-    ?>
-</div>
-<form method="get">
-<input class="submit" type="submit" value="Start over">
-</form>
-<?php
-    endif;
-    endif;
-    ?>
-
-<div class="credits">
-<h3 class="credit">Made with ❤️  by Ehud Adler</h3>
-<h4 class="credit">Big thanks to @ericshermancs</h4>
-<h4 class="credit">Please see our <a href="https://github.com/Grade-Notifier/GN-Core/blob/master/CONTRIBUTORS.md" title="Contributors">full list of contributors</a>.</h4>
-</div>
-</div>
-
-<div class="column column--right">
-<?php
-    if ($landing):
-    ?>
-<img class="image" alt="Phone" src="Assets/site/undraw_mobile_life_381t_edited.svg">
-<?php
-    elseif ($status == "ok"):
-    ?>
-<img class="image" alt="Phone with check mark" src="Assets/site/undraw_order_confirmed_1m3v.svg">
-<?php
-    elseif ($status == "error"):
-    ?>
-<img class="image" alt="Phone with exclamation mark" src="Assets/site/undraw_order_confirmed_1m3v_and_heartbroken_cble.svg">
-<?php
-    endif
-    ?>
-</div>
-</div>
+        <div class="column column--right">
+            <?php
+            if ($landing) :
+            ?>
+                <img class="image" alt="Phone" src="Assets/site/undraw_mobile_life_381t_edited.svg">
+            <?php
+            elseif ($status == "ok") :
+            ?>
+                <img class="image" alt="Phone with check mark" src="Assets/site/undraw_order_confirmed_1m3v.svg">
+            <?php
+            elseif ($status == "error") :
+            ?>
+                <img class="image" alt="Phone with exclamation mark" src="Assets/site/undraw_order_confirmed_1m3v_and_heartbroken_cble.svg">
+            <?php
+            endif
+            ?>
+        </div>
+    </div>
 </body>
+
 </html>
--- src/helper/security.py
+++ src/helper/security.py
@@ -1,33 +1,34 @@
 from Crypto.PublicKey import RSA
 from Crypto.Cipher import PKCS1_OAEP
+from Crypto.Signature import PKCS1_PSS
+from Crypto.Hash import SHA
 from Crypto import Random
-
+import Crypto
+import base64
 KEY_SIZE = 2048
 
 def gen_keys():
     key = RSA.generate(KEY_SIZE)
     private_key = key.export_key()
-    file_out = open("private.pem", "wb")
+    file_out = open("../../test_keys/private.pem", "wb")
     file_out.write(private_key)
-
+    file_out.close()
+    
     public_key = key.publickey().export_key()
-    file_out = open("public.pem", "wb")
+    file_out = open("../../test_keys/public.pem", "wb")
     file_out.write(public_key)
+    file_out.close()
+    print("WARNING:: Don't forget to update the index.php")
 
 def encrypt(message):
-    key = RSA.importKey(open('public.pem').read())
-    cipher = PKCS1_OAEP.new(key)
+    key = RSA.importKey(open('../../test_keys/public.pem').read())
+    cipher = PKCS1_OAEP.new(key, Crypto.Hash.SHA256, lambda x,y: Crypto.Signature.PKCS1_PSS.MGF1(x,y, Crypto.Hash.SHA1))
     ciphertext = cipher.encrypt(message)
     return ciphertext
 
-def decrypt(message):
-    key = RSA.importKey(open('private.pem').read())
-    cipher = PKCS1_OAEP.new(key)
-    ciphertext = cipher.decrypt(message)
-    return ciphertext
-
-gen_keys()
-cipher = encrypt(b'password')
-decrypted = decrypt(cipher)
-print(decrypted)
-
+def decrypt(ciphertext_base64):
+    key = RSA.importKey(open('../../test_keys/private.pem').read())
+    cipher = PKCS1_OAEP.new(key=key, hashAlgo=Crypto.Hash.SHA256, mgfunc=lambda x,y: Crypto.Signature.PKCS1_PSS.pss.MGF1(x,y, Crypto.Hash.SHA1))
+    ciphertext = base64.b64decode(ciphertext_base64)
+    message = cipher.decrypt(ciphertext)
+    return message
