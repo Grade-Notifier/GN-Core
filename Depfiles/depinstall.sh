761d69cc5840dd941f93e279f135d6e3d6c792b1
Ehud Adler <Huddie@users.noreply.github.com>
2019-12-25 15:46:51 -0500

    Update depinstall.sh

--- Depfiles/depinstall.sh
+++ Depfiles/depinstall.sh
@@ -13,6 +13,7 @@ done < ./Depfiles/dependencies.comp
 
 # Node
 if [[ "$OSTYPE" == "linux-gnu" ]] || [[ "$OSTYPE" == "darwin"* ]] && [ ! -d ~/.nvm ]; then
+    echo "Installing NVM.....\n";
     curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.1/install.sh | bash;
     export NVM_DIR="$HOME/.nvm"
     [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
@@ -22,6 +23,7 @@ fi
 
 # CSS/Node
 if [[ "$HOSTNAME" == "venus" ]] || [[ "$HOSTNAME" == "mars" ]] || [["$HOSTNAME" == "ubuntu-s-4vcpu-8gb-nyc1-01"]] ; then
+    echo "Setting up css.....\n";
     npm install;
     npm install gulp-cli -g;
     npm install gulp -D;
