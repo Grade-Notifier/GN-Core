30992f4520137929ee9d261e4dcfb5210dcbccb5
ericshermancs <zensap92@gmail.com>
2019-12-27 10:47:09 -0500

    fix column names again lol

--- src/core/dbsetup.py
+++ src/core/dbsetup.py
@@ -11,4 +11,4 @@ mydb = mysql.connector.connect(user=DB_USERNAME, host=DB_HOST, passwd=DB_PASSWOR
 mycursor = mydb.cursor()
 mycursor.execute('CREATE DATABASE IF NOT EXISTS GradeNotifier')
 mycursor.execute('USE GradeNotifier')
-mycursor.execute('CREATE TABLE IF NOT EXISTS Users (id INT AUTO_INCREMENT PRIMARY KEY, username VARCHAR(255), password VARCHAR(512), school VARCHAR(10), gradeHash VARCHAR(255), phoneNumber VARCHAR(255), lastChecked DATETIME NOT NULL DEFAULT "1970-01-01 00:00:00", lastUpdated DATETIME NOT NULL DEFAULT "1970-01-01 00:00:00", dateCreated DATETIME NOT NULL DEFAULT NOW())')
+mycursor.execute('CREATE TABLE IF NOT EXISTS Users (id INT AUTO_INCREMENT PRIMARY KEY, username VARCHAR(255), password VARCHAR(512), school VARCHAR(10), gradeHash VARCHAR(255), phoneNumber VARCHAR(255), lastChecked DATETIME NOT NULL DEFAULT "1970-01-01 00:00:00", lastChanged DATETIME NOT NULL DEFAULT "1970-01-01 00:00:00", dateCreated DATETIME NOT NULL DEFAULT NOW())')
